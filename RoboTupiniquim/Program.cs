ece69d16b5361be13978acf31d48ec57084fed76
davidinovargas <davidinovargas@gmail.com>
2025-03-27 16:19:00 -0300

    Add project files.

---
+++ RoboTupiniquim/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RoboTupiniquim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a posição inicial do robô em X e Y: ");
+
+
+
+            Console.WriteLine("Digite a direção inicial do robô (N, L, O, S): ");
+
+
+
+            Console.WriteLine("Digite os comandos para o robô (D, E, M): ");
+        }
+    }
+}
